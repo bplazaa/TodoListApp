fed15fd51f6bf40bf228f80a095c65abb9d4d9bd
Bryan Plaza <43506332+bplazaa@users.noreply.github.com>
2022-08-02 01:18:06 -0500

    Front diectory

---
+++ frontend/prontofront/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TaskList from './components/TaskList';
+
+function App() {
+  return (
+    <div className="task-app">
+      <TaskList/>
+    </div>
+  );
+}
+
+export default App;
